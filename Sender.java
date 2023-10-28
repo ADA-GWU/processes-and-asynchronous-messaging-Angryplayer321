bff9f3533224909a2e0c23151531667db6f0820a
Angryplayer321 <agayevayxan581@gmail.com>
2023-10-29 01:40:28 +0400

    Fetching and Threads added

--- Sender.java
+++ Sender.java
@@ -1,10 +1,46 @@
 import java.sql.*;
+import java.util.Scanner;
+import java.util.concurrent.*;
+
 
 public class Sender {
    public static void main(String args[]) {
       Connection c = ConnectToDatabase("34.76.123.81", "5432", "hw1", "dist_user", "dist_pass_123");
-      InsertIntoAsync(c, "Salam Ibo");
+      InsertIntoAsync(c, "Shukur");
+      InsertIntoAsync(c, "Salamatciliq");
+      ExecutorService executor = Executors.newFixedThreadPool(1);
+      CompletableFuture<Void> senderFuture = CompletableFuture.runAsync(() -> {
+         // Code for sending the message (replace this with your actual sending logic)
+         while(!FetchMessage(c, true))
+         {
+            try {
+               TimeUnit.SECONDS.sleep(2);
+            } catch (InterruptedException e) {
+               throw new RuntimeException(e);
+            }
+         }
+      }, executor);
+
+//      while(!FetchMessage(c))
+//      {
+//         try {
+//            TimeUnit.SECONDS.sleep(2);
+//         } catch (InterruptedException e) {
+//            throw new RuntimeException(e);
+//         }
+//      }
       SelectAllFromAsync(c);
+      Scanner sc = new Scanner(System.in);
+      System.out.println("Enter the message:");
+
+      while (true) {
+         String s =sc.nextLine();
+         if(s.equals("Exit")) {
+            executor.shutdown();
+            break;
+         }
+         else InsertIntoAsync(c, s);
+      }
    }
 
    private static Connection ConnectToDatabase(String ip,String port, String databaseName, String user, String password)
@@ -17,7 +53,6 @@ public class Sender {
                  .getConnection(url,
                          user, password);
          System.out.println("Opened database in host: " + ip +" successfully!");
-         c.setAutoCommit(false);
       } catch (Exception e) {
          System.out.println("Could not connect to database in host: " + ip);
          //e.printStackTrace();
@@ -56,6 +91,40 @@ public class Sender {
          System.out.println(sqlException.getMessage());
       }
    }
+   private static boolean FetchMessage(Connection c,boolean isEndless)
+   {
+      boolean isAllFetched = true;
+      try{
+         Statement statement = c.createStatement();
+
+         String sql = "select * from async_messages where received_time is null;";
+         ResultSet rs = statement.executeQuery(sql);
+         while ( rs.next() ) {
+            int id = rs.getInt("record_id");
+            String  name = rs.getString("sender_name");
+            String  message = rs.getString("message");
+            Timestamp sentTime = rs.getTimestamp("sent_time");
+            Timestamp receivedTime = rs.getTimestamp("received_time");
+            if(receivedTime==null) {
+               isAllFetched = false;
+               System.out.println(name + ": " + message);
+               System.out.println();
+               UpdateMessage(c, id);
+               break;
+            }
+         }
+         rs.close();
+         statement.close();
+//         if(isAllFetched) System.out.println("There is no messages.");
+         if(isEndless) return false;
+         return isAllFetched;
+      }
+      catch (SQLException sqlException)
+      {
+         System.out.println(sqlException.getMessage());
+      }
+      return false;
+   }
 
    private static void InsertIntoAsync(Connection c, String message)
    {
@@ -63,6 +132,18 @@ public class Sender {
          Statement statement = c.createStatement();
          String sql = "INSERT INTO async_messages (sender_name,message,sent_time) "
                  + "VALUES ('Ayxan', '"+ message+"', CURRENT_TIMESTAMP);";
+         statement.executeUpdate(sql);
+      }
+      catch (SQLException sqlException)
+      {
+         System.out.println(sqlException.getMessage());
+      }
+   }
+   private static void UpdateMessage(Connection c, int id)
+   {
+      try {
+         Statement statement = c.createStatement();
+         String sql = "UPDATE async_messages set received_time = current_timestamp where record_id="+id+";";
          statement.executeUpdate(sql);
       }
       catch (SQLException sqlException)
